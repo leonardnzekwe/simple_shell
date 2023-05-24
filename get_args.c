9e82b044a1f9bf81a5f4d1b5a94dcf4dc58ef549
Leonard Nzekwe <nzekweleo@gmail.com>
2023-05-24 03:05:28 +0100

    Fix Checker Errors, get_args.c

--- get_args.c
+++ get_args.c
@@ -25,6 +25,11 @@ int get_args(char *exe)
 		{
 			check_get_args(line_buffer, num_char_read);
 		}
+		else if (num_char_read == 0) /* End of File (CTRL + D) */
+		{
+			free(line_buffer);
+			exit(0);
+		}
 		else if (_strncmp(line_buffer, "exit", 3) == 0) /*exit*/
 			exit_shell(arg_count, arg_vector, line_buffer, line_buffer_dup);
 		else if (num_char_read == 4 && _strcmp(line_buffer, "env\n") == 0) /*env*/
